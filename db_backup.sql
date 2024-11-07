a54443dda639c367f142e4b997be6925c344e497
yoezequiel <ezequielcasiano15@gmail.com>
2024-11-07 16:50:58 -0300

    Backup de la base de datos antes del push

--- db_backup.sql
+++ db_backup.sql
@@ -1,101 +1,16 @@
--- MySQL dump 10.13  Distrib 8.0.39, for Linux (x86_64)
+-- MariaDB dump 10.19  Distrib 10.11.6-MariaDB, for debian-linux-gnu (x86_64)
 --
--- Host: localhost    Database: TiendaVerde
+-- Host: localhost    Database: 112358
 -- ------------------------------------------------------
--- Server version	8.0.39-0ubuntu0.20.04.1
+-- Server version	10.11.6-MariaDB-0+deb12u1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `Ensaladas`
---
-
-DROP TABLE IF EXISTS `Ensaladas`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `Ensaladas` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `Nombre` varchar(50) DEFAULT NULL,
-  `Peso` float DEFAULT NULL,
-  `Precio` float DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `Ensaladas`
---
-
-LOCK TABLES `Ensaladas` WRITE;
-/*!40000 ALTER TABLE `Ensaladas` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Ensaladas` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `Ingredientes`
---
-
-DROP TABLE IF EXISTS `Ingredientes`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `Ingredientes` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `Nombre` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `Ingredientes`
---
-
-LOCK TABLES `Ingredientes` WRITE;
-/*!40000 ALTER TABLE `Ingredientes` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Ingredientes` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `Intermedia`
---
-
-DROP TABLE IF EXISTS `Intermedia`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `Intermedia` (
-  `idEnsaladas` int DEFAULT NULL,
-  `idIngredientes` int DEFAULT NULL,
-  KEY `idEnsaladas` (`idEnsaladas`),
-  KEY `idIngredientes` (`idIngredientes`),
-  CONSTRAINT `Intermedia_ibfk_1` FOREIGN KEY (`idEnsaladas`) REFERENCES `Ensaladas` (`id`),
-  CONSTRAINT `Intermedia_ibfk_2` FOREIGN KEY (`idIngredientes`) REFERENCES `Ingredientes` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `Intermedia`
---
-
-LOCK TABLES `Intermedia` WRITE;
-/*!40000 ALTER TABLE `Intermedia` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Intermedia` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2024-11-07 16:48:05
