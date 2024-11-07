ce053a003b0f3137ebaa0252d0f58611c18ce81f
yoezequiel <ezequielcasiano15@gmail.com>
2024-11-07 16:52:15 -0300

    a

--- db_backup.sql
+++ db_backup.sql
@@ -1,6 +1,6 @@
 -- MariaDB dump 10.19  Distrib 10.11.6-MariaDB, for debian-linux-gnu (x86_64)
 --
--- Host: localhost    Database: 112358
+-- Host: localhost    Database: tiendaverde
 -- ------------------------------------------------------
 -- Server version	10.11.6-MariaDB-0+deb12u1
 
@@ -14,3 +14,100 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `ensalada_ingrediente`
+--
+
+DROP TABLE IF EXISTS `ensalada_ingrediente`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ensalada_ingrediente` (
+  `id_ensalada` int(11) NOT NULL,
+  `id_ingrediente` int(11) NOT NULL,
+  PRIMARY KEY (`id_ensalada`,`id_ingrediente`),
+  KEY `id_ingrediente` (`id_ingrediente`),
+  CONSTRAINT `ensalada_ingrediente_ibfk_1` FOREIGN KEY (`id_ensalada`) REFERENCES `ensaladas` (`id`),
+  CONSTRAINT `ensalada_ingrediente_ibfk_2` FOREIGN KEY (`id_ingrediente`) REFERENCES `ingredientes` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ensalada_ingrediente`
+--
+
+LOCK TABLES `ensalada_ingrediente` WRITE;
+/*!40000 ALTER TABLE `ensalada_ingrediente` DISABLE KEYS */;
+INSERT INTO `ensalada_ingrediente` VALUES
+(1,1),
+(1,2),
+(1,3);
+/*!40000 ALTER TABLE `ensalada_ingrediente` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ensaladas`
+--
+
+DROP TABLE IF EXISTS `ensaladas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ensaladas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) NOT NULL,
+  `precio` float NOT NULL,
+  `peso` float NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ensaladas`
+--
+
+LOCK TABLES `ensaladas` WRITE;
+/*!40000 ALTER TABLE `ensaladas` DISABLE KEYS */;
+INSERT INTO `ensaladas` VALUES
+(1,'Mixta',100,10);
+/*!40000 ALTER TABLE `ensaladas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ingredientes`
+--
+
+DROP TABLE IF EXISTS `ingredientes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ingredientes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ingredientes`
+--
+
+LOCK TABLES `ingredientes` WRITE;
+/*!40000 ALTER TABLE `ingredientes` DISABLE KEYS */;
+INSERT INTO `ingredientes` VALUES
+(1,'Lechuga'),
+(2,'Tomate'),
+(3,'Cebolla'),
+(7,'Zanahoria'),
+(8,'Cebolla2');
+/*!40000 ALTER TABLE `ingredientes` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-11-07 16:52:07
