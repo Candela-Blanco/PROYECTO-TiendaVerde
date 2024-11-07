fd999f3b160e2656aa701a48806bdf5fe45b5109
candela <candelablanco147@gmail.com>
2024-11-07 18:05:49 -0300

    Backup de la base de datos antes del push

--- db_backup.sql
+++ db_backup.sql
@@ -1,13 +1,13 @@
--- MariaDB dump 10.19  Distrib 10.11.6-MariaDB, for debian-linux-gnu (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.39, for Linux (x86_64)
 --
 -- Host: localhost    Database: tiendaverde
 -- ------------------------------------------------------
--- Server version	10.11.6-MariaDB-0+deb12u1
+-- Server version	8.0.39-0ubuntu0.20.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!50503 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -21,10 +21,10 @@
 
 DROP TABLE IF EXISTS `ensalada_ingrediente`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `ensalada_ingrediente` (
-  `id_ensalada` int(11) NOT NULL,
-  `id_ingrediente` int(11) NOT NULL,
+  `id_ensalada` int NOT NULL,
+  `id_ingrediente` int NOT NULL,
   PRIMARY KEY (`id_ensalada`,`id_ingrediente`),
   KEY `id_ingrediente` (`id_ingrediente`),
   CONSTRAINT `ensalada_ingrediente_ibfk_1` FOREIGN KEY (`id_ensalada`) REFERENCES `ensaladas` (`id`),
@@ -38,10 +38,7 @@ CREATE TABLE `ensalada_ingrediente` (
 
 LOCK TABLES `ensalada_ingrediente` WRITE;
 /*!40000 ALTER TABLE `ensalada_ingrediente` DISABLE KEYS */;
-INSERT INTO `ensalada_ingrediente` VALUES
-(1,1),
-(1,2),
-(1,3);
+INSERT INTO `ensalada_ingrediente` VALUES (1,1),(1,2),(1,3);
 /*!40000 ALTER TABLE `ensalada_ingrediente` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -51,10 +48,10 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `ensaladas`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `ensaladas` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(50) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
   `precio` float NOT NULL,
   `peso` float NOT NULL,
   PRIMARY KEY (`id`)
@@ -67,8 +64,7 @@ CREATE TABLE `ensaladas` (
 
 LOCK TABLES `ensaladas` WRITE;
 /*!40000 ALTER TABLE `ensaladas` DISABLE KEYS */;
-INSERT INTO `ensaladas` VALUES
-(1,'Mixta',100,10);
+INSERT INTO `ensaladas` VALUES (1,'Mixta',100,10);
 /*!40000 ALTER TABLE `ensaladas` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -78,10 +74,10 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `ingredientes`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `ingredientes` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(50) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -92,12 +88,7 @@ CREATE TABLE `ingredientes` (
 
 LOCK TABLES `ingredientes` WRITE;
 /*!40000 ALTER TABLE `ingredientes` DISABLE KEYS */;
-INSERT INTO `ingredientes` VALUES
-(1,'Lechuga'),
-(2,'Tomate'),
-(3,'Cebolla'),
-(7,'Zanahoria'),
-(8,'Cebolla2');
+INSERT INTO `ingredientes` VALUES (1,'Lechuga'),(2,'Tomate'),(3,'Cebolla'),(7,'Zanahoria'),(8,'Cebolla2');
 /*!40000 ALTER TABLE `ingredientes` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -110,4 +101,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-11-07 17:21:52
+-- Dump completed on 2024-11-07 18:05:49
