71fc9e2dd0865b6690be4d45beb192f48e2e5b60
yoezequiel <ezequielcasiano15@gmail.com>
2024-11-07 17:21:44 -0300

    comand sql

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 from flask import Flask
 from db import conn
-app=Flask(__name__)
 
+app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    db=conn()
-    cursor=db.cursor()
-    cursor.execute("""
-                   SELECT 
-                   """)  
-    return 
+    db = conn()
+    cursor = db.cursor()
+    cursor.execute(
+        """
+            SELECT ensaladas.id, ensaladas.nombre, ensaladas.precio, ensaladas.peso,
+            GROUP_CONCAT(ingredientes.nombre) AS ingredientes FROM ensaladas LEFT JOIN ensalada_ingrediente ON ensaladas.id = ensalada_ingrediente.id_ensalada LEFT JOIN ingredientes ON ensalada_ingrediente.id_ingrediente = ingredientes.id GROUP BY ensaladas.id;
+       """
+    )
+    return
